3d485fcaebfa8de1ed9a7b536e3aa8bc2e8a844f
Lucas Felipe <lucas.miranda880@outlook.com>
2025-02-22 23:14:18 -0300

    testtest

---
+++ Fase 5/main.java
@@ -0,0 +1,27 @@
+public class main {
+    public static void main (String[] args) {
+    
+        //tipos primitivos
+        //byte, short, int, long 
+        int idade;
+        idade = 10;
+        System.out.println(idade);
+        //float, double
+        double altura = 1.75;
+        float altura2 = 1.75f;
+        System.out.println(altura);
+        System.out.println(altura2);
+        //char
+        char letra = 'a';
+        System.out.println(letra);
+        //boolean
+        boolean verdade = true;
+        System.out.println(verdade);
+
+        //Tipo de referencia
+        //string
+        String nome = "Lucas"; //para tipo referencia a letra inicial é maiuscula e para string, a informaçao fica entre aspas duplas
+        System.out.println(nome);
+
+    }
+}
