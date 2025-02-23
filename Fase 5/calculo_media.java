c0dc8818808b2179dd9eeb93c9d58bae9a06914b
Lucas Felipe <lucas.miranda880@outlook.com>
2025-02-22 23:31:29 -0300

    first java code lines

---
+++ Fase 5/calculo_media.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class calculo_media {
+    public static void main (String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite o valor da primeira nota: ");
+        double nota1 = scanner.nextDouble();
+        System.out.println("Digite o valor da segunda nota: ");
+        double nota2 = scanner.nextDouble();
+
+        double media = (nota1+nota2)/2;
+
+        System.out.println("A média é: " + media);
+}
+   
+}
---
+++ Fase 5/calculo_operadores.java
@@ -0,0 +1,3 @@
+public class calculo_operadores {
+    
+}
