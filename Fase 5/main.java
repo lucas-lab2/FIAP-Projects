59a98f2ce612676e67a93492db348ec6963d738c
Lucas Felipe <lucas.miranda880@outlook.com>
2025-02-25 00:45:32 -0300

    operadores lógics e relacionais adicionados

--- Fase 5/main.java
+++ Fase 5/main.java
@@ -23,5 +23,13 @@ public class Main{
         String nome = "Lucas"; //para tipo referencia a letra inicial é maiuscula e para string, a informaçao fica entre aspas duplas
         System.out.println(nome);
 
+        int a = 5;
+        int b = 10;
+
+        System.out.println(a > b);   // Isso retorna falso, já que 'a' não é maior que 'b'
+        System.out.println(a <= b);  // Isso retorna verdadeiro, já que 'a' é menor ou igual a 'b'
+        System.out.println(a == b);  // Isso retorna falso, já que 'a' não é igual a 'b'
+        System.out.println(a != b);  // Isso retorna verdadeiro, já que 'a' é diferente de 'b'
+
     }
 }
